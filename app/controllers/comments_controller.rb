f27302861a59283e49730d18b4a524c6d6673498
takakuga <sazanami398@gmail.com>
2021-11-11 14:14:50 +0900

    コメント削除のアクションを定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,11 @@ class CommentsController < ApplicationController
     redirect_to "/tweets/#{comment.tweet.id}"
   end
 
+  def destroy
+    Comment.find_by(id: params[:id], tweet_id: params[:tweet_id]).destroy
+    redirect_back(fallback_location: root_path)
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
