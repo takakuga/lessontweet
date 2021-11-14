56eba12bbf43315257e68b8cbb871806b6ab5ddf
takakuga <sazanami398@gmail.com>
2021-11-14 14:41:03 +0900

    exampleを整理

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
   describe 'ユーザー新規登録' do
+    it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
+      
+    end
     it 'nicknameが空では登録できない' do
       @user.nickname = ''
       @user.valid?
@@ -14,6 +17,16 @@ RSpec.describe User, type: :model do
       @user.email = ''
       @user.valid?
       expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+    it 'passwordが空では登録できない' do
+    end
+    it 'passwordとpassword_confirmationが不一致では登録できない' do
+    end
+    it 'nicknameが7文字以上では登録できない' do
+    end
+    it '重複したemailが存在する場合登録できない' do
+    end
+    it 'passwordが5文字以下は登録できない' do
     end
   end
 end
